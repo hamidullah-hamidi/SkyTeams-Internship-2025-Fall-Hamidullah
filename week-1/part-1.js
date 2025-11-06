8d97def6ed5b4374696fd5e168df503833c21127
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-11-06 16:47:31 +0430

    part-1 practice

--- week-1/part-1.js
+++ week-1/part-1.js
@@ -22,4 +22,42 @@ const checkNumber = (num) => {
   return 'prime';
 };
 
-console.log(checkNumber(21));
+// console.log(checkNumber(21));
+
+const sum = () => {
+  const arr = [1, 2, 3, 4];
+  let sum = 0;
+
+  for (const e of arr) {
+    sum += e;
+  }
+
+  return sum;
+};
+
+// console.log(sum());
+
+const reverse = (str) => {
+  let reverse = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+
+  return reverse;
+};
+
+// console.log(reverse('hamid'));
+
+// Array, List, Object, Map
+
+// Array
+const restaurantsName = ['50 50', 'char-fasl', 'mehman'];
+
+const restaurantsDetails = [
+  { name: '50 50', city: 'herat' },
+  { name: 'char-fasl', city: 'kabul' },
+  { name: 'mehman', city: 'herat' },
+];
+
+const restaurantsMap = new Map();
